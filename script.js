de4ea9f93d2182cd704d82b149cca06f83891321
Ebiarun08 <141803817+Ebiarun08@users.noreply.github.com>
2023-12-08 10:50:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+var nameerror = document.getElementById('name-error');
+var phoneerror = document.getElementById('phone-error');
+var emailerror = document.getElementById('email-error');
+var messageerror = document.getElementById('message-error');
+var submiterror = document.getElementById('submit-error');
+
+function validateName(){
+    var name =document.getElementById('username').value;
+
+    if(name.length == 0){
+        nameerror.innerHTML ='Name is required'
+        return false;
+    }
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameerror.innerHTML='Write a fullname'
+        return false;
+    }
+    nameerror.innerHTML = '<i class="fa-solid fa-check-to-slot"></i>'
+    return true;
+}
+function validatePhone(){
+    var phone1 = document.getElementById('phone');
+    var phone=phone1.value.trim()
+    
+    if (phone.length == 0){
+        phoneerror.innerHTML ='phone num required';
+        return false; 
+    }
+    if(phone.length != 10){
+        phoneerror.innerHTML = 'Enter 10dig num';
+        return false
+    }
+    if (!phone.match(/^[0-9]{10}$/)){
+        phoneerror.innerHTML = 'only numbers'
+        return false
+    }
+    phoneerror.innerHTML ='<i class="fa-solid fa-check-to-slot"></i>'
+    return true;
+}
+function validateEmail(){
+    var email =document.getElementById('email').value;
+
+
+    if(email.length == 0){
+        emailerror.innerHTML = 'E-mail is requierd';
+        return false;
+    }
+    if(!email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
+        emailerror.innerHTML ='Enter valid Email';
+        return false;
+    }
+    emailerror.innerHTML='<i class="fa-solid fa-check-to-slot"></i>';
+    return true
+}
+function validateMessage(){
+    var message =document.querySelector('#message').value;
+
+    if(message.length == 0){
+        messageerror.innerHTML ='Type something';
+        return false;
+
+    }
+    if( message.length<=20){
+        messageerror.innerHTML = 'Type atleast 20 charectors';
+        return false
+    }
+    messageerror.innerHTML='<i class="fa-solid fa-check-to-slot"></i>';
+    return true;
+
+}
